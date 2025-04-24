79c25f0889ae860362159168a93e5418c007bba3
yellow-dash <shinobuki3770@gmail.com>
2025-04-25 04:53:22 +0900

    wrap in function

--- GetRedditPost.py
+++ GetRedditPost.py
@@ -2,21 +2,22 @@ import os
 import praw
 from dotenv import load_dotenv
 
-load_dotenv("api.env")
-
-client_id = os.getenv("reddit_client_id")
-client_secret = os.getenv("reddit_client_secret")
-user_agent = os.getenv("reddit_user_agent")
-
-reddit = praw.Reddit(
-    client_id=client_id,
-    client_secret=client_secret,
-    user_agent=user_agent,
-)
-
-print(reddit.read_only)
-
-for submission in reddit.subreddit("tifu").hot(limit=2):
-    if submission.is_self and not submission.stickied:
-        print(submission.title)
-        print(submission.selftext)
+def get_top_reddit_posts(subreddit,time_filter="day",limit=10):
+    load_dotenv("reddit_api.env")
+    client_id = os.getenv("reddit_client_id")
+    client_secret = os.getenv("reddit_client_secret")
+    user_agent = os.getenv("reddit_user_agent")
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent,
+    )
+    top_reddit_posts = []
+    for submission in reddit.subreddit(subreddit).top(time_filter="day",limit=limit):
+        post = []
+        if submission.is_self and not submission.stickied:
+            post.append(submission.title)
+            post.append(submission.selftext)
+            top_reddit_posts.append(post)
+    
+    print(top_reddit_posts)
