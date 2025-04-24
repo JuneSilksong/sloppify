be68e25814c332a738e7ce7a4960145421a31ad7
yellow-dash <shinobuki3770@gmail.com>
2025-04-25 05:19:04 +0900

    typed function and added docstring to GetRedditPost.py

--- GetRedditPost.py
+++ GetRedditPost.py
@@ -1,23 +1,41 @@
 import os
 import praw
 from dotenv import load_dotenv
+from typing import List, Tuple
 
-def get_top_reddit_posts(subreddit,time_filter="day",limit=10):
+def get_top_reddit_posts(
+    subreddit: str,
+    time_filter: str = "day",
+    limit: int = 10
+) -> List[Tuple[str, str]]:
+    
+    """
+    Fetches the top text-based reddit posts from a specific subreddit
+
+    Args:
+        subreddit (str): The name of the subreddit (e.g., 'tifu', 'aitah').
+        time_filter (str): The time filter for top posts (e.g., 'hour', 'day', 'week', 'month', etc.).
+        limit (int): The number of top posts to fetch.
+    
+    Returns:
+        top_reddit_posts (List[Tuple[str, str]]): A list of tuples, each containing the title and selftext of a post.
+    """
+    
     load_dotenv("reddit_api.env")
     client_id = os.getenv("reddit_client_id")
     client_secret = os.getenv("reddit_client_secret")
     user_agent = os.getenv("reddit_user_agent")
+
     reddit = praw.Reddit(
         client_id=client_id,
         client_secret=client_secret,
         user_agent=user_agent,
     )
-    top_reddit_posts = []
-    for submission in reddit.subreddit(subreddit).top(time_filter="day",limit=limit):
-        post = []
+
+    top_reddit_posts: List[Tuple[str, str]] = []
+
+    for submission in reddit.subreddit(subreddit).top(time_filter=time_filter,limit=limit):
         if submission.is_self and not submission.stickied:
-            post.append(submission.title)
-            post.append(submission.selftext)
-            top_reddit_posts.append(post)
+            top_reddit_posts.append((submission.title, submission.selftext))
     
-    print(top_reddit_posts)
+    return top_reddit_posts
