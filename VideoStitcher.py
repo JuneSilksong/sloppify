d8fd45f9ad8da6d3da588d539d99f3e45a52e978
yellow-dash <shinobuki3770@gmail.com>
2025-04-25 18:58:19 +0900

    Set random start time for video

--- VideoStitcher.py
+++ VideoStitcher.py
@@ -1,26 +1,31 @@
 from moviepy.editor import VideoFileClip, AudioFileClip, CompositeVideoClip, CompositeAudioClip
 from typing import List, Tuple
+import random
 
 def stitch_video(video_file: str, music_file: str, tts_audio_file: str): #add tts_srt: str later
     
+    # Initialise, resize, and crop video to fit 9:16 aspect ratio
     video = VideoFileClip(video_file)
-    resized = video.resize(height=1080)
-    cropped = resized.crop(x_center=resized.w/2, width=607)
+    resized = video.resize(height=1440)
+    cropped = resized.crop(x_center=resized.w/2, width=810)
 
-    tts = AudioFileClip(tts_audio_file).volumex(0.6)
-    music = AudioFileClip(music_file).volumex(0.07)
-    music = music.set_duration(tts.duration).audio_fadeout(1)
+    # Initialise and combine tts audio and music
+    tts_audio = AudioFileClip(tts_audio_file).volumex(0.6)
+    music = AudioFileClip(music_file).volumex(0.05)
+    music = music.set_duration(tts_audio.duration).audio_fadeout(1)
+    audio = CompositeAudioClip([music,tts_audio])
 
-    audio = CompositeAudioClip([music,tts])
-
-    final = cropped.set_audio(audio)
     duration = min(audio.duration, cropped.duration, 15)
+
+    start_time = random.randint(1,int(video.duration-duration-10))
+    cropped = cropped.subclip(start_time)
+    final = cropped.set_audio(audio)   
     final = final.subclip(0, duration)
 
-    final.write_videofile("output_9x16.mp4", codec="libx264", audio_codec="aac", fps=30)
+    final.write_videofile("output_9x16.mp4", codec="libx264", audio_codec="aac", fps=60)
 
-video_file = "youtube_minecraft_parkour.mp4"
-music_file = "youtube_joyful_chess.mp3"
-tts_audio_file = "TIFU_TIFU by asking what a guy who hates me said about me in a group chat_part1.mp3"
+#video_file = "youtube_minecraft_parkour_1440p.webm"
+#music_file = "youtube_joyful_chess.mp3"
+#tts_audio_file = "TIFU_TIFU by asking what a guy who hates me said about me in a group chat_part1.mp3"
 
-stitch_video(video_file,music_file,tts_audio_file)
+#stitch_video(video_file,music_file,tts_audio_file)
