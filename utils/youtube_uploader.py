f825ac2f6a1c992513e5807d472a080dd08c21af
yellow-dash <shinobuki3770@gmail.com>
2025-04-25 21:35:54 +0900

    Save/load credentials to reduce need to manually authorize

--- utils/youtube_uploader.py
+++ utils/youtube_uploader.py
@@ -1,16 +1,32 @@
 import os
+import pickle
 import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request
+
+def get_authenticated_service():
+    scopes = ["https://www.googleapis.com/auth/youtube.upload"]
+    credentials = None
+    
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            credentials = pickle.load(token)
+
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file("client_secrets.json", scopes)
+            credentials = flow.run_local_server(port=8080)
+        with open("token.pickle", "wb") as token:
+            pickle.dump(credentials, token)
+    
+    return build("youtube", "v3", credentials=credentials)
 
 def upload_to_youtube(file_path, title, description, tags=[], categoryId="22", privacyStatus="public"):
 
-    scopes = ["https://www.googleapis.com/auth/youtube.upload"]
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file("client_secrets.json", scopes)
-    
-    credentials = flow.run_local_server(port=8080)
-
-    youtube = build("youtube", "v3", credentials=credentials)
+    youtube = get_authenticated_service()
 
     request = youtube.videos().insert(
         part="snippet,status",
@@ -31,10 +47,12 @@ def upload_to_youtube(file_path, title, description, tags=[], categoryId="22", p
     response = request.execute()
     print("Video uploaded:", response["id"])
 
-"""upload_to_youtube(
+"""
+upload_to_youtube(
     file_path="output/test.mp4",
     title="test_title",
     description="test_description",
     tags=["reddit","tifu","tts","tiktok"],
     privacyStatus="private"
-)"""
+)
+"""
