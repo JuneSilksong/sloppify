8b165b6eed4f516c1bd756db7e2350b33e41de9f
Skiffee <justintayza@gmail.com>
2025-05-06 22:38:48 -0400

    made config.py for easy adjustments

---
+++ config.py
@@ -0,0 +1,24 @@
+# config.py
+
+# Post grabbing
+POST_LIMIT = 2
+TIME_FRAME = "week" # Options: "day", "week", "month", "year"
+
+# Folders
+AUDIO_OUTPUT_FOLDER = "input/tts"
+SUBTITLE_OUTPUT_FOLDER = "input/srt"
+BACKGROUND_VIDEO = "youtube_minecraft_parkour_1440p-001.mp4"
+BACKGROUND_MUSIC = "youtube_joyful_chess.mp3"
+
+# Voice settings
+VOICE_ID = "pNInz6obpgDQGcFmaJgB"
+TTS_MODEL_ID = "eleven_flash_v2"
+TTS_OUTPUT_FORMAT = "mp3_44100_128"
+
+# Reddit API env file
+REDDIT_ENV_FILE = "reddit.env"
+ELEVENLABS_ENV_FILE = "eleven_labs.env"
+
+# Other constants
+MAX_CHARS = 5000
+UPLOAD_LOG = "upload_log.txt"
